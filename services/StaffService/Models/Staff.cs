ae7e9949a0f6c7eae5f06932a03b27c6e173f938
StronDay <76127547+StronDay@users.noreply.github.com>
2026-01-23 10:14:36 +0000

    Add Staff model

---
+++ services/StaffService/Models/Staff.cs
@@ -0,0 +1,12 @@
+namespace StaffService;
+
+public class Staff
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? SecondName { get; set; }
+    public int Age { get; set; }
+    public string? JobPosition { get; set; }
+    public int PhoneNumber { get; set; }
+    public string? Email { get; set; }
+}
